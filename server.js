9f2856a1d7b2be223e443bbde81366ea91d284ea
Moses Buta <moses.buta@yahoo.com.au>
2021-03-10 12:52:35 +1100

    new url

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://zelibrarytest.herokuapp.com/"
 };
 
 app.use(cors(corsOptions));
