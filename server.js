43ddd3f1217b0a204abba7162b8fd96914382ad8
Moses Buta <moses.buta@yahoo.com.au>
2021-03-10 14:42:56 +1100

    new url

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://vigorous-sinoussi-0d0c2e.netlify.app/"
+  origin: "https://zeibrary.netlify.app/"
 };
 
 app.use(cors(corsOptions));
